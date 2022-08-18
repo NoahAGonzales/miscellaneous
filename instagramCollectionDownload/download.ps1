451b481885b88b04f5b625ed565af9aaeba3ca72
Noah Gonzales <noahagonzo@yahoo.com>
2022-08-17 20:13:56 -0500

    finished

--- instagramCollectionDownload/download.ps1
+++ instagramCollectionDownload/download.ps1
@@ -5,22 +5,25 @@
 # If the value supplied for workingPath is already in the path, it will be removed as part of the cleanup for this script
 # To avoid credential handling, you must already be logged into instagram on Google Chrome
 
+# .\instagramCollectionDownload\download.ps1 "C:\Users\noaha\Downloads\miscellaneous\instagramCollectionDownload" "https://www.instagram.com/noahagonzo/saved/todo/17874368231545673/" "C:\Users\noaha\Downloads\miscellaneous\instagramCollectionDownload\instagram_noahagonzo.xml" 
+
 param(
     # The working directory containing chromedriver.exe and WebDriver.dll
     [Parameter(Mandatory=$true)]$workingPath,
-    # The directory downloaded posts will be saved in. A folder in this directory for each collection downloaded
-    [Parameter(Mandatory=$true)]$downloadDirectory,
     # Comma separated list of URLs of the collections to download
     [Parameter(Mandatory=$true)]$collectionURLs,
     # Path to encrypted credentials - Created encrypted credentials xml file with  Get-Credential | Export-Clixml -Path "path"
     [Parameter(Mandatory=$true)]$credentialsPath
 )
 
+$debug = $false
+
 $loadWaitTime = 10
+$videoProcessingWaitTime = 15
+$actionWaitTime = 1
 
 # Add the working directory to the environment path.
 # This is required for the ChromeDriver to work.
-$envTemp=$env:Path
 if (($env:Path -split ';') -notcontains $workingPath) {
     $env:Path += ";$workingPath"
 }
@@ -35,15 +38,18 @@ $ChromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
 $credentials = Import-Clixml $credentialsPath
 
 # Login
+if ($debug) {"DEBUG: Navigating to instagram"}
 $ChromeDriver.Navigate().GoToURL("https://www.instagram.com/accounts/login/")
 Start-Sleep -seconds $loadWaitTime
 try {
     $ChromeDriver.findElement([OpenQA.Selenium.By]::Name("username")).SendKeys($credentials.Username)
     $ChromeDriver.findElement([OpenQA.Selenium.By]::Name("password")).SendKeys($credentials.GetNetworkCredential().Password)
+    Start-Sleep -seconds $actionWaitTime
     $ChromeDriver.findElement([OpenQA.Selenium.By]::Name("password")).SendKeys([OpenQA.Selenium.Keys]::Enter)
 }
 catch {
     # Exit if error entering credential to avoid entering credentials elsewhere
+    "Error entering credentials - exiting" | Write-Output
     Exit
 }
 
@@ -51,48 +57,73 @@ Start-Sleep -seconds $loadWaitTime
 
 $collectionURLs = $collectionURLs.split(',')
 foreach ($URL in $collectionURLs) {
+    # Navigate to collection
     $ChromeDriver.Navigate().GoToURL($URL)
-
     Start-Sleep -seconds $loadWaitTime
 
+    # Get Collection linkssa
     $collectionLinks = $ChromeDriver.FindElements([OpenQA.Selenium.By]::CssSelector("a"))
     $postLinks = @()
-    # Get post links
+    # Get post links from collection links
     foreach ($element in $collectionLinks) {
         if ($element.getAttribute("href") -like "*instagram.com/p/*") {
             $postLinks += $element.getAttribute("href")
         }
     }
 
-    $ChromeDriver.Navigate().GoToURL("https://toolzu.com/downloader/instagram/video/")
+    # Output post links
+    '' + $postLinks.Count + " posts to download:" | Write-Output
+    $postLinks | Write-Output
+    "---" | Write-Output
+
+    # Go to download site
+    if ($debug) {"DEBUG: Navigating to download site"}
+    $ChromeDriver.Navigate().GoToURL("https://igpanda.com/")
+    Start-Sleep -seconds $loadWaitTime
+
+    # Click on type
+    if ($debug) {"DEBUG: Selecting type"}
+    $ChromeDriver.FindElement([OpenQA.Selenium.By]::XPath('//*[@id="url-form"]/div[1]/div/div/div/div[2]/a')).Click()
+    Start-Sleep -seconds $actionWaitTime
+
     # Download each post
     foreach ($postLink in $postLinks) {
         # Enter link
-        $ChromeDriver.FindElement([OpenQA.Selenium.By]::Id("instagramdownloaderform-search")).SendKeys($postLink)
-        $ChromeDriver.FindElement([OpenQA.Selenium.By]::Id("instagramdownloaderform-search")).SendKeys([OpenQA.Selenium.Keys]::Enter)
+        if ($debug) {"DEBUG: Entering post link"}
+        $urlTextField = $ChromeDriver.FindElement([OpenQA.Selenium.By]::Id("url"))
+        $urlTextField.SendKeys($postLink)
+        $urlTextField.SendKeys([OpenQA.Selenium.Keys]::Enter)
 
-        # Select download button
-
-        # Enter Windows dialog text
-
-        Start-Sleep -seconds 30
+        # Site downloading video may take some time
+        Start-Sleep -seconds $videoProcessingWaitTime
 
         # Clear text field
-        $ChromeDriver.FindElement([OpenQA.Selenium.By]::Id("instagramdownloaderform-search")).SendKeys([OpenQA.Selenium.Keys]::Control + "A")
-        Start-Sleep -seconds 1
-        $ChromeDriver.FindElement([OpenQA.Selenium.By]::Id("instagramdownloaderform-search")).SendKeys([OpenQA.Selenium.Keys]::Delete)
-        Start-Sleep -seconds 1
-        
+        if ($debug) {"DEBUG: Clearing text field"}
+        $urlTextField.SendKeys([OpenQA.Selenium.Keys]::Control + "A")
+        Start-Sleep -seconds $actionWaitTime
+        $urlTextField.SendKeys([OpenQA.Selenium.Keys]::Delete)
+        Start-Sleep -seconds $actionWaitTime
+
+        try {
+            if ($debug) {"DEBUG: Clicking on download button"}
+            # Click download button
+            $downloadButton = $ChromeDriver.FindElement([OpenQA.Selenium.By]::XPath(('//*[@id="home"]/div/div[1]/div/div/div/div/div/div/div/div/a')))
+            $downloadButton.Click()
+            Start-Sleep -seconds $actionWaitTime
+        }
+        catch {
+            # Error downloading video
+            "Error downloading $postLink" | Write-Output
+            continue
+        }
+
+        Start-Sleep -seconds $actionWaitTime
     }
-    
-    Start-Sleep -seconds $loadWaitTime
 }
 
-Start-Sleep -seconds 10
+# Wait for last download
+Start-Sleep -seconds 30
 
 # Cleanup driver
 $ChromeDriver.Close()
 $ChromeDriver.Quit()
-
-# Restore path
-#$env:path = $envTemp
